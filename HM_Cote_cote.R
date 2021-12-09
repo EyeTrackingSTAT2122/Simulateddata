03fdf715ebc3d276aba3cc3975f8634a96c100cf
Elias <eliashermance@gmail.com>
2021-12-09 15:36:39 +0100

    Script génération HM plateaux

--- HM_Cote_cote.R
+++ HM_Cote_cote.R
@@ -65,7 +65,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.95
     img = "Repas/Plateau_redim/plateau1.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 2){
@@ -74,7 +73,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.95
     img = "Repas/Plateau_redim/plateau2.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 3){
@@ -83,7 +81,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.95
     img = "Repas/Plateau_redim/plateau3.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 4){
@@ -92,7 +89,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.95
     img = "Repas/Plateau_redim/plateau4.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 5){
@@ -101,7 +97,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.25
     img = "Repas/Plateau_redim/plateau5.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 6){
@@ -110,7 +105,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.25
     img = "Repas/Plateau_redim/plateau6.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 7){
@@ -119,7 +113,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.25
     img = "Repas/Plateau_redim/plateau7.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else{
@@ -128,7 +121,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     p_dessert = 0.25
     img = "Repas/Plateau_redim/plateau8.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   for (i in 1:N_juges){
@@ -137,7 +129,6 @@ plateau_knowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_reg
     
     cotcot <- ggplot(bvn1, aes(x = bvn1_X1, y = bvn1_X2))  + 
       theme_void() +
-      # annotation_custom(imgH) +
       annotation_custom(imgR) +
       stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE, alpha = 0.7) +
       scale_fill_distiller(palette= "Spectral", direction=-1,guide = "none") + 
@@ -182,49 +173,41 @@ plateau_unknowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_r
   if (n_plateau == 1){
     img = "Repas/Plateau_redim/plateau1.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 2){
     img = "Repas/Plateau_redim/plateau2.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 3){
     img = "Repas/Plateau_redim/plateau3.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 4){
     img = "Repas/Plateau_redim/plateau4.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 5){
     img = "Repas/Plateau_redim/plateau5.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 6){
     img = "Repas/Plateau_redim/plateau6.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else if (n_plateau == 7){
     img = "Repas/Plateau_redim/plateau7.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   else{
     img = "Repas/Plateau_redim/plateau8.png"
     img <- readPNG(img)
-    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
     imgR <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 1)
   }
   for (i in 1:N_juges){
@@ -233,7 +216,6 @@ plateau_unknowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_r
     
     cotcot <- ggplot(bvn1, aes(x = bvn1_X1, y = bvn1_X2))  + 
       theme_void() +
-      # annotation_custom(imgH) +
       annotation_custom(imgR) +
       stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE, alpha = 0.7) +
       scale_fill_distiller(palette= "Spectral", direction=-1,guide = "none") + 
@@ -258,4 +240,226 @@ plateau_unknowed <- function(n_plateau = 1, N_juges = 100,p_regard_C = 0.33, p_r
 
 for (j in 1:8){
   plateau_unknowed(n_plateau = j)
+}
+
+plateau_sup_knowed <- function(n_plateau = 1, N_juges = 100, p_regard_C = 0.33, p_regard_A = 0.66, width_size = 256, height_size = 201){
+  if (n_plateau == 1){
+    p_carrot = 0.95
+    p_assiette = 0.95
+    p_dessert = 0.95
+    img = "Repas/plateau_redim/plateau1.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 2){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau2.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 3){
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau3.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 4){
+    p_carrot = 0.25
+    p_assiette = 0.25
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau4.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 5){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau5.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 6){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau6.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 7){
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau7.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, x = 0.5, y = 0.5, hjust = 0)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else{
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau8.png"
+    img <- readPNG(img)
+    imgH <- rasterGrob(img, interpolate=TRUE, hjust = 0.5)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  for (i in 1:N_juges){
+    tirage <- runif(1)
+    ifelse(test = tirage < p_regard_C, yes = carrot(), no = ifelse(test = tirage < p_regard_A, yes =  assiette(), no = dessert()))
+    
+    cotcot <- ggplot(bvn1, aes(x = bvn1_X1, y = bvn1_X2))  + 
+      theme_void() +
+      # annotation_custom(imgH) +
+      annotation_custom(imgR) +
+      stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE, alpha = 0.7) +
+      scale_fill_distiller(palette= "Spectral", direction=-1,guide = "none") + 
+      scale_x_continuous(limits=c(198,530))+
+      scale_y_continuous(limits=c(33, 255))+
+      coord_fixed(xlim = c(160,555),ylim = c(0,288)) +
+      theme(legend.background = element_blank(),
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text = element_blank())
+    
+    tirage_2 <- runif(1)
+    
+    #Si il trouve le plateau équilibré
+    if ((tirage_2 < p_carrot & tirage < p_regard_C)| (tirage_2 < p_assiette & tirage < p_regard_A & tirage > p_regard_C)|(tirage_2 < p_dessert & tirage > p_regard_A)){
+      png(file = paste0("data/plateau_superpose/Connu/Balanced/","Juge_",i,"_plateau_n",n_plateau,".png"), 
+          width = width_size, 
+          height = height_size)
+      plot(cotcot)
+      dev.off()
+    }
+    
+    #Si il trouve le plateau déséquilibré
+    if ((tirage_2 > p_carrot & tirage < p_regard_C)| (tirage_2 > p_assiette & tirage < p_regard_A & tirage > p_regard_C)| (tirage_2 > p_dessert & tirage > p_regard_A)){
+      png(file = paste0("data/plateau_superpose/Connu/Unbalanced/","Juge_",i,"_plateau_n",n_plateau,".png"), 
+          width = width_size, 
+          height = height_size)
+      plot(cotcot)
+      dev.off()
+    }
+    # save heatmaps in .png format
+  }
+}
+
+
+for (j in 1:8){
+  plateau_sup_knowed(n_plateau = j)
+}
+
+plateau_sup_unknowed <- function(n_plateau = 1, N_juges = 100, p_regard_C = 0.33, p_regard_A = 0.66, width_size = 256, height_size = 201){
+  if (n_plateau == 1){
+    p_carrot = 0.95
+    p_assiette = 0.95
+    p_dessert = 0.95
+    img = "Repas/plateau_redim/plateau1.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 2){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau2.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 3){
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau3.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 4){
+    p_carrot = 0.25
+    p_assiette = 0.25
+    p_dessert = 0.95
+    img = "Repas/Plateau_redim/plateau4.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 5){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau5.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 6){
+    p_carrot = 0.95
+    p_assiette = 0.5
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau6.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else if (n_plateau == 7){
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau7.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  else{
+    p_carrot = 0.25
+    p_assiette = 0.75
+    p_dessert = 0.25
+    img = "Repas/Plateau_redim/plateau8.png"
+    img <- readPNG(img)
+    imgR <- rasterGrob(img, interpolate=TRUE, x = 1, y = 0.5, hjust = 1)
+  }
+  for (i in 1:N_juges){
+    tirage <- runif(1)
+    ifelse(test = tirage < p_regard_C, yes = carrot(), no = ifelse(test = tirage < p_regard_A, yes =  assiette(), no = dessert()))
+    
+    cotcot <- ggplot(bvn1, aes(x = bvn1_X1, y = bvn1_X2))  + 
+      theme_void() +
+      # annotation_custom(imgH, xmin = 160, xmax = 525) +
+      annotation_custom(imgR) +
+      stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE, alpha = 0.7) +
+      scale_fill_distiller(palette= "Spectral", direction=-1,guide = "none") + 
+      scale_x_continuous(limits=c(198,530))+
+      scale_y_continuous(limits=c(33, 255))+
+      coord_fixed(xlim = c(160,555),ylim = c(0,288)) +
+      theme(legend.background = element_blank(),
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text = element_blank())
+    
+    tirage_2 <- runif(1)
+    
+    #Si il trouve le plateau équilibré
+    png(file = paste0("data/plateau_superpose/Inconnu/","Juge_",i,"_plateau_n",n_plateau,".png"), 
+        width = width_size, 
+        height = height_size)
+    plot(cotcot)
+    dev.off()
+    # save heatmaps in .png format
+  }
+}
+
+i = "test" 
+
+for (j in 1:8){
+  plateau_sup_unknowed(n_plateau = j)
 }
